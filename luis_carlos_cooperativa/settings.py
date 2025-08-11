c7f0646df2138c7eaf6c63349aa28eabe4e5f637
Kevin <iguarankevin53@email.com>
2025-08-11 13:23:04 -0500

    Alpha hp 1

--- luis_carlos_cooperativa/settings.py
+++ luis_carlos_cooperativa/settings.py
@@ -91,12 +91,6 @@ WSGI_APPLICATION = 'luis_carlos_cooperativa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
