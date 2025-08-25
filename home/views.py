bb771f6ce8c9f0981c18ca6de1db7f75171f4c85
Kevin <iguarankevin53@email.com>
2025-08-25 09:43:17 -0500

    Mejoras visuales

--- home/views.py
+++ home/views.py
@@ -1,21 +1,15 @@
-from django.shortcuts import render, redirect
-from django.utils.translation import gettext_lazy as _  
-
-from datetime import date
-
+from django.shortcuts import render
 from shop.models import Product, Category, ShowBestOffers
-
 
 
 def home_view(request):
     categories_list = Category.objects.all()
-
     best_offers = ShowBestOffers.objects.filter(active=True)
 
     category_products = {}
 
     for category in categories_list:
-        products = Product.objects.filter(category=category)
+        products = Product.objects.filter(category=category).order_by('-sales')[:4]
         if products.exists():
             category_products[category] = products
 
@@ -27,7 +21,5 @@ def home_view(request):
     return render(request, 'pages/home/home.html', context)
 
 
-
 def about_view(request):
     return render(request, 'pages/home/about.html')
-
